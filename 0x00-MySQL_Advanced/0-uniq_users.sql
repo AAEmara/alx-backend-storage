5b0c6aa42cd28e126537e3ceba01ee185132b383
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-12 11:46:55 +0000

    0x00: Task0 SQL QUERY.
    Created the `holberton` database and also
    created the `users` table.

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Creating a table named `users` in the database created "holberton".
+CREATE DATABASE IF NOT EXISTS `holberton`;
+USE `holberton`;
+CREATE TABLE IF NOT EXISTS `users`
+(
+id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255)
+);
