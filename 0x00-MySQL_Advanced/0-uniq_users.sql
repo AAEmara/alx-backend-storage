073391ea64b3e953bb788f3b7a04fb0a2b45cdde
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-13 07:33:17 +0000

    0x00: Task0 SQL QUERY - Output Fix.
    Removed any query statement related to table
    creation.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,4 @@
 -- Creating a table named `users` in the database created "holberton".
-CREATE DATABASE IF NOT EXISTS `holberton`;
-USE `holberton`;
 CREATE TABLE IF NOT EXISTS `users`
 (
 id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
