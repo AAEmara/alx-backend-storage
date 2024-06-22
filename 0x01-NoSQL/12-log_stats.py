bfddfbf2a4e86291b03c86c599f83c52c0488c8f
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-21 19:07:07 -0700

    0x01: Task12 Python Script - Count Documents.
    Used `count_documents` operation that counts the
    number of documents with a filter if provided.

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""A module that provide stats about Nginx log stored in MongoDB."""
+
+from pymongo import MongoClient
+
+
+client = MongoClient()
+db = client.logs
+nginx = db.nginx
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+methods_log = dict()
+
+logs_count = nginx.count_documents({})
+for method in methods:
+    methods_log[method] = nginx.count_documents({"method": method})
+status_count = nginx.count_documents({"path": "/status"})
+
+print(f"{logs_count} logs")
+print("Methods:")
+for key, val in methods_log.items():
+    print(f"\tmethod {key}: {val}")
+print(f"{status_count} status check")
