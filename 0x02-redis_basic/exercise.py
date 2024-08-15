1aa6db9e873994f2f732e47856b34c261351649d
AAEmara <abdelrahmanemara18@gmail.com>
2024-08-15 03:49:38 +0300

    0x02: Task1 Python Script - Redis GET & Convert.
    Applied three methods to convert the value
    retrieved from string bytes to the desired data
    type while preserving the redis.get method
    behavior.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -26,3 +26,40 @@ class Cache:
         self._redis.set(str(random_key), data)
 
         return (str(random_key))
+
+    def get(
+            self,
+            key: str, fn: Optional[Callable] = None
+           ) -> Union[str, int, bytes, None]:
+        """Converts the string bytes into the desired data type and returns it.
+        Args:
+            key: The key for the desired value.
+            fn: The method to be used to convert the data type of the value.
+        Returns:
+            The converted value in the desired data type.
+        """
+        value: Union[bytes, None] = self._redis.get(key)  # Normal Redis GET.
+        if (value is None or fn is None):
+            return value  # The value is returned in bytes.
+        converted_value = fn(value)  # Converting the value using a method.
+        return (converted_value)  # Returning the converted value.
+
+    def get_str(self, value: bytes) -> str:
+        """Convert the string bytes into string data type and returns it.
+        Args:
+            value: The value for the desired value.
+        Returns:
+            The converted value as a string data type.
+        """
+        string_value = value.decode(encoding='utf-8', errors='strict')
+        return string_value
+
+    def get_int(self, value: bytes) -> int:
+        """Convert the string bytes into integer data type and returns it.
+        Args:
+            value: The key for the desired value.
+        Returns:
+            The converted value as an integer data type.
+        """
+        integer_value = int(value)
+        return integer_value
